78678d180de5b9469e7db264078c18389c0aa383
Abdulrahman Al-Ruwaishan <sanyemaa@gmail.com>
2018-04-27 10:17:44 -0400

    Basic structure of an electron app

---
+++ main.js
@@ -0,0 +1,8 @@
+const electron = require('electron')
+
+//module that controls app life
+const app = electron.app
+
+//module to create native browser window
+const BrowserWindow = electron.BrowserWindow
+
